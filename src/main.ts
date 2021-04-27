5ed314c2e31bea4dbac913e100688f65432ca8ab
Arseny Pavlenko <arsenyp@wix.com>
2021-04-27 10:22:00 +0300

    [=]

--- src/main.ts
+++ src/main.ts
@@ -82,8 +82,7 @@ async function run(): Promise<void> {
       )
     }
 
-    // eslint-disable-next-line no-console
-    console.log('response.data', response.data)
+    core.info(`response.data - ${JSON.stringify(response.data)}`)
 
     // Get the changed files from the response payload.
     const files = response.data.files
